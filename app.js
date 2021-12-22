e310ee80910a838c4d86e217d5c2807f71f3c227
AFRIAULAR <aularafrica@gmail.com>
2021-12-21 21:53:49 -0300

    agregando funcion autosParaLaVenta

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const concesionaria = {
       let autoFinded = autos.find(autos => autos.patente == patente);
            return autoFinded !== undefined ? autoFinded : null
    } ,
-venderAuto: function (patente) {
-    const autos = this.buscarAuto(patente);
-    if (autos) {
-      autos.vendido = true;
-    } } 
- };
-    
+    venderAuto: function (patente) {
+        const autos = this.buscarAuto(patente);
+            if (autos) {
+            autos.vendido = true;
+    } },
+    autosParaLaVenta: function (patente){
+        return autos = this.autos.filter(function (auto){
+           return !auto.vendido
+        }) 
+ }  }
